bcffc368a62314a2b0a1444a9c99aaf1d44ecc4c
SuyKingsleigh <suyan.m@aluno.ifsc.edu.br>
2019-09-23 17:13:41 -0300

    timer2 iniciado, a ideia eh reduzir el reduzindo bits e usando uma fsm pra somar tudo

--- A2_2019_2_restored/timer2.vhd
+++ A2_2019_2_restored/timer2.vhd
@@ -5,21 +5,31 @@ use IEEE.numeric_std.all;
 entity timer2 is
     port (
         clk, reset : in std_logic; 
-        sec_out : out std_logic_vector(6 downto 0);
-        min_out : out std_logic_vector(6 downto 0);
-        hour_out : out std_logic_vector(5 downto 0);
+        sec_out : out std_logic_vector(5 downto 0); -- 59 unsigned = 111 011
+        min_out : out std_logic_vector(5 downto 0);
+        hour_out : out std_logic_vector(4 downto 0); -- 23 unsigned = 10 111
     );
 end entity timer2;
 
 architecture rtl of timer2 is
     type state is (
-        count_sec, count_min, count_hour
+        count_sec_st, count_min_st, count_hour_st
     );
 
     signal pr_state, nx_state : state;
+    signal sec_count, min_count :  unsigned(sec_out'range);
+    signal hour_count : unsigned(hour_out'range);
 
 begin
-    
-    
+    process(clk, reset)
+        if reset='1' then
+            sec_count <= (others=>'0');
+            min_count <= (others=>'0');
+            hour_count <= (others=>'0');
+            pr_state <= count_sec_st; -- comeca contando os segundos 
+        elsif(clk'event and clk='1') then
+            pr_state <= nx_state; 
+        end if ;
+    end process;
     
 end architecture rtl;
